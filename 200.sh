99e5122ee2ad15811a7d7269bfd431e2b504773e
Maksym Obukhovskyi <97797231+Soubi8@users.noreply.github.com>
2022-06-24 18:32:08 +0200

    Updated 200.sh

--- 200.sh
+++ 200.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 command () {
-    response=$(curl --write-out %{http_code} --silent --output /dev/null http://3.72.251.197/articles)
+    response=$(curl --write-out %{http_code} --silent --output /dev/null https://dev.epam.pp.ua/articles)
 }
 
 n=0
